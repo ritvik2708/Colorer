0ab594811101f0f24cd02484a18fe5b988232871
ritvik <mittalritvik.rm@gmail.com>
2019-07-17 17:08:29 +0530

    Updates

---
+++ precache-manifest.db271e3c2706707f0a33115658c8d13d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23ded5fc8565df3e4d6182d16725b9c8",
+    "url": "/Colorer/index.html"
+  },
+  {
+    "revision": "3ba373396734b60a6f3e",
+    "url": "/Colorer/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "a91796ba6cdd50d98d70",
+    "url": "/Colorer/static/css/main.b14474c6.chunk.css"
+  },
+  {
+    "revision": "3ba373396734b60a6f3e",
+    "url": "/Colorer/static/js/2.43204517.chunk.js"
+  },
+  {
+    "revision": "a91796ba6cdd50d98d70",
+    "url": "/Colorer/static/js/main.6ddcbb9d.chunk.js"
+  },
+  {
+    "revision": "2c3c9bfb012b10d41dee",
+    "url": "/Colorer/static/js/runtime~main.7ed73085.js"
+  }
+]);
